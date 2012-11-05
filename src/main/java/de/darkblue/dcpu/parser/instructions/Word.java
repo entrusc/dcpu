5ce4e2a6ab86ca9efffd702df612a42d9fa2ab55
Florian Frankenberger <f.frankenberger@darkblue.de>
2012-11-05 20:47:37 +0100

    + added license header

--- src/main/java/de/darkblue/dcpu/parser/instructions/Word.java
+++ src/main/java/de/darkblue/dcpu/parser/instructions/Word.java
@@ -1,8 +1,20 @@
-/**
- * Copyright 2012 by Darkblue Inh. Florian Frankenberger
+/*
+ * Copyright (C) 2012 Florian Frankenberger <f.frankenberger@darkblue.de>
  *
- * Be inspired by this source but please don't copy it ;)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.darkblue.dcpu.parser.instructions;
 
 import java.io.DataInputStream;
